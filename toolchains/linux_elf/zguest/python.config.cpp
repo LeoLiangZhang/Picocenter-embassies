416bae92531e57988fef1d0ae55c44e0ec28178a
Liang Zhang <liang@ccs.neu.edu>
2014-09-16 12:39:47 -0400

    Patch posix emulation for Apache.

---
+++ toolchains/linux_elf/zguest/python.config.cpp
@@ -0,0 +1,20 @@
+#include "RunConfig.h"
+#include "Zone.h"
+
+void config_create(RunConfig *rc)
+{
+  // Zone *zone;
+  // fprintf(stderr, "Hello world 2\n");
+  Zone *zone = new Zone("apache_httpd",	// just a label for debugging inside zguest
+  			20,			// MB of brk()-accessible address space.
+  			ZOOG_ROOT "/toolchains/linux_elf/python/bin/python2.7"
+  			// path to the -pie binary you want to run.
+  			);
+  zone->add_arg("-B");
+  zone->add_arg("/home/liang/Works/embassies/pseudofiles/main.py");
+  rc->add_zone(zone);
+  
+  // rc->add_vnc();
+  // zone = rc->add_app("hello");
+
+}
