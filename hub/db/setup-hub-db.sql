4091d0e44b63aa3e30c9c7766c00d4ea574dab44
Frank Cangialosi <frank@cs.umd.edu>
2015-04-05 23:58:50 -0400

    finished hub db setup script

--- hub/db/setup-hub-db.sql
+++ hub/db/setup-hub-db.sql
@@ -12,4 +12,4 @@ CREATE INDEX pico_index ON picoports (pico_id) USING BTREE;
 CREATE TABLE ips (worker_id INT NOT NULL, ip VARCHAR(15) NOT NULL, status TINYINT NOT NULL);
 CREATE INDEX worker_index ON ips (worker_id) USING BTREE;
 
-SELECT ip from ips WHERE ip NOT IN (SELECT public_ip FROM picos p, picoports pp WHERE p.pico_id=pp.pico_id AND (pp.port=443 OR pp.port=6667 OR pp.port=80));
+CREATE TABLE meta (last_internal_ip VARCHAR(15) NOT NULL);
